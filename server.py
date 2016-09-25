7fd56c79199bdf1b28ef11ea278cdc6a25534f07
Dor Rondel <dor.rondel@gmail.com>
2016-09-25 06:33:01 -0400

    finished my part

--- server.py
+++ server.py
@@ -2,10 +2,20 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+eeg_array = []
+
 @app.route('/hey/', methods=['GET', 'POST'])
 def index():
-	print(request.args)
-	return "hey girlll".format()
+
+	if(any(request.args) != False):
+		eeg_array.append("{}, {}, {}, {}".format(request.args['ch1'],
+			 request.args['ch2'],
+			 request.args['ch3'],
+			 request.args['ch4']
+		))
+		return "{}".format(eeg_array[-10:])
+	return "{}".format(eeg_array[-10:])
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=8000)
